33cc509e62b15ebbf3d263f2680b9b2e4e769285
KaatCAIBXL <kaat.dbolle@gmail.com>
2025-11-15 01:11:24 +0100

    Create models.py

---
+++ app/models.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel
+from typing import List, Dict, Optional
+
+class Segment(BaseModel):
+    start: float
+    end: float
+    text: str
+
+class TranslationSegment(Segment):
+    language: str
+
+class VideoMetadata(BaseModel):
+    id: str
+    filename: str
+    original_language: str
+    sentence_pairs: List[Segment]
+    translations: Dict[str, List[TranslationSegment]]  # bv. "en": [...]
+
+class VideoListItem(BaseModel):
+    id: str
+    filename: str
+    available_subtitles: List[str]
+    available_dubs: List[str]
